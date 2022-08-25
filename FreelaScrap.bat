1184aaec0f1efaf6acbe93896e57882e51e6c4e4
Felipe Rosene Klapsztein <feliperosenek@gmail.com>
2022-08-25 09:34:49 -0300

    config

---
+++ FreelaScrap.bat
@@ -0,0 +1,3 @@
+start cmd.exe /c node app.js
+
+
---
+++ app.js
@@ -0,0 +1,148 @@
+const puppeteer = require('puppeteer');
+const jsdom = require("jsdom");
+const {
+  JSDOM
+} = jsdom;
+const https = require("https")
+var cmd = require('node-cmd');
+const { Console } = require('console');
+const api = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const fs = require('fs');
+const { ifError } = require('assert');
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+async function freelaWebscrap() {
+
+  try {
+    let options = {
+      defaultViewport: {
+        width: 1366,
+        height: 768,
+      },
+      headless: true,
+    };
+
+    const optionsHook = {
+      hostname: "937bb71b3cf5cf99e2b37c0985e9a62a.m.pipedream.net",
+      port: 443,
+      path: "/",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+
+    let browser = await puppeteer.launch(options);
+    let newJob = 0;
+    var arrayDescJob = [];
+    var arrayLinkJob = [];
+    var arrayTimeJob = [];
+    var arrayTitleJob = [];
+
+    var t = 0;
+    while (t < 1) {
+
+      let page = await browser.newPage();
+      let page2 = await browser.newPage();
+
+      page.goto("https://www.workana.com/jobs?category=it-programming&language=pt");
+      await delay(10000)
+
+      pageData = await page.evaluate(() => document.querySelector('*').outerHTML);
+      dom = new JSDOM(pageData);
+
+      titleJob = dom.window.document.querySelectorAll(".h3.project-title");
+      timeJob = dom.window.document.querySelectorAll("span[class='date']");
+      descJob = dom.window.document.querySelectorAll(".html-desc.project-details");
+      linkJob = dom.window.document.querySelectorAll(".h3.project-title > a");
+
+            var timestamp = new Date();
+            var dia = timestamp.getDate();
+            var hora = timestamp.getHours()
+            var minuto = timestamp.getMinutes()
+
+            if(hora <=9){hora="0"+hora}
+            if(minuto <=9){minuto="0"+minuto}
+            if(dia <=9){dia="0"+dia}
+            var hour = hora+":"+minuto;    
+     
+      for (var i = 0; i < titleJob.length; i++) {
+        
+        timeJobV = timeJob[i].title;
+        timeJobV = timeJobV.split("de");
+        jobTimeDay = timeJobV[0];
+        jobTimeHour =  timeJobV[2];
+        jobTimeHour = jobTimeHour.split(" ")
+
+        jobTimeHour = jobTimeHour[2].split(":");
+        jobTimeH = jobTimeHour[0]-3;
+        jobTimeM = jobTimeHour[1]
+        
+        if(jobTimeH <=9){jobTimeH="0"+jobTimeH}
+        calcH = hour.split(":");
+        calcHour = jobTimeH - calcH[0]
+        calcM= calcH[1] - jobTimeM; 
+
+        if(calcHour == "-1"){
+        calcM = 60 - parseInt(jobTimeM) + parseInt(calcH[1]);         
+        }
+
+        if(parseInt(jobTimeDay) == parseInt(dia) && parseInt(calcM) < 15){
+          if(parseInt(calcM) < 0){calcM = 0;}
+      
+          arrayTitleJob.push(titleJob[i].textContent);
+          arrayTimeJob.push(calcM);
+          arrayDescJob.push(descJob[i].textContent);
+          arrayLinkJob.push(linkJob[i].href)
+
+          var titleFilter = titleJob[i].textContent
+          var descFilter = descJob[i].textContent
+          var url =  linkJob[i].href          
+         
+          if(titleFilter.includes("delphi") === false || titleFilter.includes("android") === false || titleFilter.includes("página de vendas") === false || titleFilter.includes("aplicativo ") === false || titleFilter.includes("power bi") === false || titleFilter.includes("extensão chrome") 
+          === false || titleFilter.includes("e-commerce") === false || titleFilter.includes("React") === false || titleFilter.includes("nuvemshop") === false || titleFilter.includes("instagram") === false || titleFilter.includes("aviator") 
+          === false || titleFilter.includes("ios") === false || titleFilter.includes("facebook")  === false || titleFilter.includes("excel") === false || titleFilter.includes("ads") === false || titleFilter.includes("bet356") 
+          ){
+          cmd.run('start www.workana.com' + url);
+          }
+                    
+         }
+      }
+
+      const data = {
+        "titleJob": arrayTitleJob,
+        "timeJob": arrayTimeJob,
+        "descJob": arrayDescJob,
+        "linkJob": arrayLinkJob
+      }
+   
+       const express = api();
+
+       express.use(morgan('dev'))
+       express.use(api.json())
+       express.use(cors());
+
+
+       express.get('/jobs', (req,res) => {
+           return res.json(data)
+      })
+
+      // express.listen(14000, () => {
+      //     console.log("Express ON");
+      // })
+
+      page.close();
+      page2.close();
+      await delay(900000)
+
+    }
+
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+freelaWebscrap();
